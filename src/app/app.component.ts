00b73fb1ba045cb7dcfc23e6150c1e6b6f6150b9
Cesar Zaragoza <zaragoza0711@gmail.com>
2025-01-26 20:31:20 -0600

    final practica

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PaisajeComponent } from "./paisaje/paisaje.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PaisajeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
